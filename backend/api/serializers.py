74b94ff456f6a53df8b35e3f4f71f860ec4793b0
xororist <hcachon@myges.fr>
2024-04-15 00:18:28 +0200

    Update a user's informations

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -12,6 +12,14 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+    
+    def update(self, instance, validated_data):
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.username = validated_data.get('username', instance.username)
+        instance.email = validated_data.get('email', instance.email)
+        instance.save()
+        return instance
 
 class ProgramSerializer(serializers.ModelSerializer):
     class Meta:
--- backend/api/views.py
+++ backend/api/views.py
@@ -54,3 +54,33 @@ class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+
+class UserRetrieveView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        pk = self.kwargs.get("pk")
+        if pk is None:
+            return None
+        return self.get_queryset().filter(pk=pk).first()
+
+
+class UserUpdateView(generics.UpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return User.objects.filter(pk=user.pk) 
+    
+    def perform_update(self, serializer):
+        if serializer.is_valid():
+            serializer.save()
+        else:
+            print(serializer.errors)
+
+    def get_object(self):
+        return self.get_queryset().first()
