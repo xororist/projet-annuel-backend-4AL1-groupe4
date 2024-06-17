1c1feb9a831e71156f59cb0c8b3b4498aee384ad
xororist <hcachon@myges.fr>
2024-06-17 17:06:26 +0200

    Updating compose.yaml for local development with access to the db

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'dev-database',
         'USER': 'postgres',
         'PASSWORD': 'N97sl?@!!Xnoa',
-        'HOST': 'db',  
+        'HOST': '127.0.0.1',
         'PORT': '5432',
     }
 }
