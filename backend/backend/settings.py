7bf0fb84fcff7da3dcda9287ca71b4e3d832667e
xororist <hcachon@myges.fr>
2024-07-24 23:53:42 +0200

    removed static dir

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -112,11 +112,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
-if not DEBUG:    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
